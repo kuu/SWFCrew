b0d7c1508b926ab9ebf172de59bb7f52d9faf5df
Jason Parrott <jason.parrott@gree.co.jp>
2012-08-31 17:58:56 +0900

    Added license stuff

--- src/actions/removeobject.js
+++ src/actions/removeobject.js
@@ -1,3 +1,9 @@
+/**
+ * @author Jason Parrott
+ *
+ * Copyright (C) 2012 Jason Parrott.
+ * This code is licensed under the zlib license. See LICENSE for details.
+ */
 (function(global) {
 
   var mActions = theatre.define('theatre.crews.swf.actions');
