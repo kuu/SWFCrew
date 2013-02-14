a70f6db7eeba912cb7529372c99ea10fc7a8208d
Kuu MIYAZAKI <kuu.miyazaki@gree.net>
2013-02-14 19:28:51 +0900

    Implemented EditText (glyph font.)

--- src/handlers/EditTexts.js
+++ src/handlers/EditTexts.js
@@ -9,305 +9,133 @@
   var theatre = global.theatre;
   var mSWFCrew = theatre.crews.swf;
   var mHandlers = mSWFCrew.handlers;
-  var mShapeUtils = mSWFCrew.utils.shape;
-  //var TextActor = mSWFCrew.actors.TextActor;
-  //var TextProp = mSWFCrew.props.TextProp;
+  var EditTextRenderProp = mSWFCrew.props.EditTextRenderProp;
+  var Canvas = global.benri.draw.Canvas;
+  var Font = global.benri.draw.Font;
+  var TextStyle = global.benri.draw.TextStyle;
+  var CanvasRenderable = global.benri.render.CanvasRenderable;
 
-  function generateGlyphEditTextDrawFunction(pActor, pParams) {
-    var i, il, j, jl, tTextBounds = pActor.bounds,
-        tCurrX = pActor.leftmargin, tXBounds,
-        tString = pActor.text + '',
-        tFont = pActor.font,
-        tTwipsWidth = 0, tTwipsHeight = 0, tEMHeight = 0,
-        tTextLines = new Array(), tYPadding = pActor.leading;
-
-    tXBounds = tTextBounds.right - tTextBounds.left - pActor.leftmargin - pActor.rightmargin;
-
-    var tDrawFunctions = new Array(), tPaddingList = new Array();
-
-    for (i = 0, il = tString.length; i < il; i++) {
-      var tCharCode = tString.charCodeAt(i),
-          tFontInfo = tFont.lookupTable[tCharCode + ''],
-          tAdvance = tFontInfo ? (tFontInfo.advance * pActor.fontheight / 1024)
-                      : (tCharCode < 256 ? pActor.fontheight / 2 : pActor.fontheight);
-
-      if (pActor.multiline && (tCharCode === 10 || tCharCode === 13 || (tFontInfo && tCurrX + tAdvance > tXBounds))) {
-        // new line
-        tYPadding += (pActor.leading + pActor.fontheight);
-        tTextLines.push({draws: tDrawFunctions, paddings: tPaddingList, height: pActor.fontheight, emHeight: tEMHeight});
-        tTwipsWidth = Math.max(tTwipsWidth, tCurrX);
-        tDrawFunctions = new Array();
-        tPaddingList = new Array();
-        tCurrX = pActor.leftmargin;
-        tEMHeight = 0;
-      }
-      if (tCharCode === 10 || tCharCode === 13 || !tFontInfo) {
-        continue;
-      }
-      var tShape = tFontInfo.shape;
-      tShape.bounds = {left: 0, right: 1024,
-        top: (tFont.ascent === 0 ? -1024 : -tFont.ascent),
-        bottom: (tFont.descent === 0 ? 0 : tFont.descent)};
-      tShape.fillStyles[0].color = pActor.textcolor;
-      var tActualBounds = {left: 0, right: 0, top: 0, bottom: 0};
-      var tDrawFunc = mShapeUtils.generateDrawFunction(null, tShape, tActualBounds);
-      if (tActualBounds.bottom > tShape.bounds.bottom) {
-        // The font's shape can exceed the EM square (1024 x 1024) downward.
-        tShape.bounds.bottom = tActualBounds.bottom;
-        tDrawFunc = mShapeUtils.generateDrawFunction(null, tShape);
-      }
-      tDrawFunctions.push(tDrawFunc);
-      tPaddingList.push({x: tCurrX / 20, y: tYPadding / 20});
-      tEMHeight = Math.max(tEMHeight, (tShape.bounds.bottom - tShape.bounds.top));
-      tCurrX += tAdvance;
-    }
-    tTextLines.push({draws: tDrawFunctions, paddings: tPaddingList, height: pActor.fontheight, emHeight: tEMHeight});
-    tTwipsWidth = Math.max(tTwipsWidth, tCurrX);
-    tTwipsHeight = tYPadding + pActor.leading + pActor.fontheight;
-
-    pParams.width = tTwipsWidth;
-    pParams.height = tTwipsHeight;
-    pParams.string = tString;
-
-    return function (pData) {
-        var tContext = pData.context;
-
-        for (i = 0, il = tTextLines.length; i < il; i++) {
-          var tDrawList = tTextLines[i].draws;
-          var tPadding = tTextLines[i].paddings;
-          var tFontScale = tTextLines[i].height / tTextLines[i].emHeight;
-          //var tXScale = tTextLines[i].xScale;
-          //var tYScale = tTextLines[i].yScale;
-//console.log('tXScale=' + tXScale);
-//console.log('tYScale=' + tYScale);
-          this.drawingContext.save();
-          this.drawingContext.scale(tFontScale, tFontScale);
-          for (j = 0, jl = tDrawList.length; j < jl; j++) {
-            tContext.save();
-            tContext.translate(tPadding[j].x, tPadding[j].y);
-//console.log('tContext.translate(', tPadding[j].x, tPadding[j].y, ');');
-            tDrawList[j].call(this, pData);
-            tContext.restore();
-          }
-          this.drawingContext.restore();
-        }
-      };
+  function createFont(pSwfFont, pDeviceText) {
+    var tFont = new Font();
+    tFont.name = pSwfFont.name;
+    tFont.advance = pSwfFont.name;
+    tFont.ascent = pSwfFont.ascent;
+    tFont.descent = pSwfFont.descent;
+    tFont.leading = pSwfFont.leading;
+    tFont.italic = pSwfFont.italic;
+    tFont.bold = pSwfFont.bold;
+    tFont.system = pDeviceText;
+    return tFont;
   }
 
-  function generateDeviceEditTextDrawFunction(pActor, pParams) {
-    var tProps = pActor.getProps('Drawing');
-    var tContext = tProps[0].drawingContext;
-    var tColor = pActor.textcolor;
-    var tStringList, tString = pActor.text + '';
-    var tFont = pActor.font;
-    var tFontString = (tFont.italic ? 'italic ' : '') + (tFont.bold ? 'bold ' : '') + pActor.fontheight + 'px ' + tFont.name;
-    var tBounds = pActor.bounds;
-    var tXPos = pActor.leftmargin, tYPos = 0, tWidth = tBounds.right - tBounds.left - pActor.leftmargin - pActor.rightmargin, tHeight = tBounds.bottom - tBounds.top;
-    var tAlign = '\'left\'';
-    var i, il;
-
-    // CnvasRenderingContext2D.fillText() forcibly converts all the spaces into ASCII spaces.
-    // However, the space characters are sometimes used for making visual space.
-    // So, here we do such the conversion more precise way so that we can preserve the original layout.
-    tContext.save();
-    tContext.font = tFontString;
-    var tSpaceWidth = tContext.measureText('\u0020').width;
-    var tIdeographicSpaceWidth = tContext.measureText('\u3000').width;
-    var tSpaceMultRate = (tSpaceWidth && tIdeographicSpaceWidth ? tIdeographicSpaceWidth / tSpaceWidth : 4);
-
-    // This function takes an arbitrary number of the ideographic spaces (U+3000,)
-    // and returns how many ASCII spaces (U+0020) are needed for filling the same on-screen area
-    // that the ideographic spaces would have ocupied.
-    var howManySpaces = function (pString) {
-      return Math.round(pString.length * tSpaceMultRate);
-    };
-    tString = tString.replace(/\u3000+/g, function (pMatched) {
-        // Generating sucessive SPACE characters.
-        return Array(howManySpaces(pMatched) + 1).join('\u0020');
-      });
-
-    if (pActor.multiline) {
-      // Folding the text.
-      var tCharCode, tStringBuffer = '';
-      tStringList = [];
-      for (i = 0, il = tString.length; i < il; i++) {
-        tCharCode = tString.charCodeAt(i);
-        if (tCharCode === 10 || tCharCode === 13) {
-          tStringList.push(tStringBuffer);
-          tStringBuffer = '';
-          continue;
-        }
-        tStringBuffer += tString[i];
-        if (i === il - 1
-          || (tContext.measureText(tStringBuffer + tString[i + 1]).width > tWidth)) {
-          tStringList.push(tStringBuffer);
-          tStringBuffer = '';
-        }
-      }
-    } else {
-      tStringList = [tString.replace(/[\n\r]/g, '')];
-    }
-
-    if (pActor.align === 1) {
-      tAlign = '\'right\'';
-      tXPos += tWidth;
-    } else if (pActor.align === 2) {
-      tAlign = '\'center\'';
-      tXPos += tWidth / 2;
-    }
-
-    var tCode = [
-        'var tContext = pData.context;',
-        'var tTempCanvas = this.drawingCanvas;',
-        'var tTempContext = this.drawingContext;',
-        'tTempContext.save();',
-        'tTempContext.setTransform(1, 0, 0, 1, 0, 0);',
-        'tTempContext.clearRect(0, 0, tTempCanvas.width, tTempCanvas.height);',
-        'tTempContext.restore();',
-        'tTempContext.save();',
-        'tTempContext.globalCompositeOperation = \'xor\';',
-        'tTempContext.fillStyle = \'' + tColor.toString() + '\';',
-        'tTempContext.font = \'' + tFontString + '\';',
-        'tTempContext.textBaseline = \'top\';',
-        'tTempContext.textAlign = ' + tAlign + ';'
-      ];
-    for (i = 0, il = tStringList.length; i < il; i++) {
-      tString = tStringList[i].replace(/(?!\\)'/, '\\\'');
-      tCode.push(
-        'tTempContext.fillText(\'' + tString + '\', ' + tXPos + ', ' + tYPos + ', ' + tWidth + ');'
-      );
-      tYPos += (pActor.leading + pActor.fontheight);
-    }
-    tCode.push(
-        'tContext.drawImage(tTempCanvas, 0, 0);',
-        'tTempContext.restore();'
-    );
-    var tDrawFunc = new Function('pData', tCode.join('\n'));
-
-    pParams.width = tWidth;
-    pParams.height = tHeight;
-
-    tContext.restore();
-
-    return function (pData) {
-        tDrawFunc.call(this, pData);
-      };
+  function createTextStyle(pEditText, pFont) {
+    var tTextStyle = new TextStyle(pFont);
+    tTextStyle.fontHeight = Math.floor(pEditText.fontheight / 20);
+    tTextStyle.leftMargin = Math.floor((pEditText.leftmargin || 0) / 20);
+    tTextStyle.rightMargin = Math.floor((pEditText.rightmargin || 0) / 20);
+    tTextStyle.topMargin = Math.floor((pEditText.leading || 0) / 20);
+    var tAscent = pFont.ascent ? pFont.ascent : 1024;
+    tTextStyle.topMargin += Math.floor(tAscent * tTextStyle.fontHeight / 1024);
+    tTextStyle.align = pEditText.align === 1 ? 'right' : (pEditText.align === 2 ? 'center' : 'left');
+    tTextStyle.wrap = !!pEditText.wrap;
+    tTextStyle.multiline = !!pEditText.multiline;
+    return tTextStyle;
   }
 
   /**
-   * Handles SWF Texts (DefineEditText.)
-   * @param {quickswf.Text} pText The Text to handle.
+   * Handles SWF EditTexts.
+   * @param {quickswf.structs.EditText} pText The EditText to handle.
    */
-  mHandlers['DefineEditText1'] = function(pEditText) {
-    var tDictionaryToActorMap = this.actorMap;
-    var tFonts = this.swf.fonts;
-    // Define TextProp
-    var tTextPropClass = function BuiltinEditTextProp(pBackingContainer, pWidth, pHeight, pDeviceText) {
-      this.base(pBackingContainer, pWidth, pHeight, pDeviceText);
-      this.rebuildGlyph = true;
-    }
-    theatre.inherit(tTextPropClass, TextProp);
-    var tProto = tTextPropClass.prototype;
-    var tParams = new Object();
-    if (!pEditText.font) {
-      throw new Exception('No font info.');
-    }
-    var tDeviceText = !pEditText.useoutline;
-    tProto.preDraw = function(pData) {
-        if (this.rebuildGlyph && this.actor.text) {
-          var tParams = new Object();
-          if (tDeviceText) {
-            tProto.draw = generateDeviceEditTextDrawFunction(this.actor, tParams);
-          } else {
-            tProto.draw = generateGlyphEditTextDrawFunction(this.actor, tParams);
-          }
-          this.rebuildGlyph = false;
-        }
-        var tWillDraw = TextProp.prototype.preDraw.call(this, pData);
-
-/*
-        var tBounds = this.actor.bounds;
-        var tWidth  = tBounds.right  - tBounds.left;
-        var tHeight = tBounds.bottom - tBounds.top;
-        var tWidthDiff  = tParams.width  - tWidth;
-        var tHeightDiff = tParams.height - tHeight;
-        tBounds.left   -= tWidthDiff  / 2;
-        tBounds.right  += tWidthDiff  / 2;
-        tBounds.top    -= tHeightDiff / 2;
-        tBounds.bottom += tHeightDiff / 2;
-*/
-
-        return tWillDraw;
-      };
+  mHandlers['DefineEditText'] = function(pEditText) {
+    var tId = pEditText.id;
     var tBounds = pEditText.bounds;
-    var tTwipsWidth  = tBounds.right  - tBounds.left;
+    var tTwipsWidth = tBounds.right  - tBounds.left;
     var tTwipsHeight = tBounds.bottom - tBounds.top;
+    var tPixelWidth = Math.round(tTwipsWidth / 20);
+    var tPixelHeight = Math.round(tTwipsHeight / 20);
 
-    var tCanvas = tProto.drawingCanvas = global.document.createElement('canvas');
-    tCanvas.width = ((tTwipsWidth / 20) >>> 0) || 1;
-    tCanvas.height = ((tTwipsHeight / 20) >>> 0) || 1;
-    //console.log('TextProp: Canvas size : w=' + tCanvas.width + ', h=' + tCanvas.height);
+    // Create a new Canvas to render to.
+    var tCanvas = new Canvas(tPixelWidth, tPixelHeight);
+    var tSWF = this.swf;
+    var tFontId = pEditText.font;
+    var tSwfFont = tSWF.fonts[tFontId];
+    var tDeviceText = !pEditText.useoutline;
+    var tFont, tStyle;
 
-    var tContext = tProto.drawingContext = tCanvas.getContext('2d');
-    tContext.lineCap = 'round';
-    tContext.lineJoin = 'round';
-    tContext.scale(0.05, 0.05);
+    // Create font.
+    if (!(tFont = this.getFontCache(tFontId))) {
+      tFont = createFont(tSwfFont, tDeviceText);
+      this.setFontCache(tFontId, tFont);
+    }
 
-    // Define TextActor
-    var tMedia = this.swf.mediaLoader;
-    var tTextActor = tDictionaryToActorMap[pEditText.id] = function BuiltinEditTextActor(pPlayer) {
-      this.base(pPlayer);
+    // Create style.
+    tStyle = createTextStyle(pEditText, tFont);
 
-      // Copy necesary data.
-      if (pEditText.sjis && pEditText.initialtext) {
-        this.text = tMedia.get('text', pEditText.initialtext);
-      } else {
-        this.text = pEditText.initialtext;
-      }
-      this.bounds = pEditText.bounds;
-      this.leftmargin = pEditText.leftmargin;
-      this.rightmargin = pEditText.rightmargin;
-      this.align = pEditText.align;
-      this.textcolor = pEditText.textcolor;
-      this.fontheight = pEditText.fontheight;
-      this.leading = pEditText.leading;
-      this.multiline = pEditText.multiline;
-      this.font = tFonts[pEditText.font];
-      this.isDeviceText = tDeviceText;
+    /**
+     * @class
+     * @extends {theatre.crews.swf.actors.ShapeActor}
+     */
+    var BuiltinEditTextActor = this.actorMap[tId] = (function(pSuper) {
+      function BuiltinEditTextActor(pPlayer) {
+        pSuper.call(this, pPlayer);
 
-      // Creates Prop objects.
-      var tTextProp = this.prop = new tTextPropClass(pPlayer.backingContainer, this.width, this.height); // TODO: This feels like a hack...
-      this.addProp(tTextProp);
+        // Initialize text.
+        if (pEditText.sjis && pEditText.initialtext) {
+          this.text = tSWF.mediaLoader.get('text', pEditText.initialtext);
+        } else {
+          this.text = pEditText.initialtext;
+        }
+        this.device = tDeviceText;
+        this.fontHeight = pEditText.fontheight;
+        this.lookupTable = tSwfFont.lookupTable;
+        this.mediaLoader = tSWF.mediaLoader;
+        this.font = tFont;
+        this.style = tStyle;
+        this.color = pEditText.textcolor;
+        var tTextProp = new EditTextRenderProp(this.pixelWidth, this.pixelHeight);
+        this.addProp(tTextProp);
 
-      // Sets up variable accessor methods.
-      var tVarName = pEditText.variablename;
-      if (tVarName) {
-        this.varName = tVarName;
-        var tThis = this;
-        var tGetter = function () {
+        // Set up variable accessor methods.
+        var tVarName = pEditText.variablename;
+        if (tVarName) {
+          this.varName = tVarName;
+          var tThis = this;
+          var tGetter = function () {
             return tThis.text;
           };
-        var tSetter = function (pValue) {
+          var tSetter = function (pValue) {
             // need some escape ?
             tThis.text = pValue;
-            tTextProp.clearCache();
+            //tTextProp.clearCache(); TODO:
             tTextProp.rebuildGlyph = true;
             tThis.invalidate();
           };
-        this.on('enter', function () {
+          this.on('enter', function () {
             // Registers the accessor methods.
             this.parent.hookVariable(tVarName, tGetter, tSetter);
           });
-        this.on('leave', function () {
+          this.on('leave', function () {
             // Unregisters the accessor methods.
             if (this.varName && this.parent) {
               this.parent.unhookVariable(this.varName, tGetter, tSetter);
             }
           });
+        }
       }
-    };
-    theatre.inherit(tTextActor, TextActor);
-    tTextActor.prototype.bounds = pEditText.bounds;
+
+      BuiltinEditTextActor.prototype = Object.create(pSuper.prototype);
+      BuiltinEditTextActor.prototype.constructor = BuiltinEditTextActor;
+
+      BuiltinEditTextActor.prototype.bounds = tBounds;
+      BuiltinEditTextActor.prototype.pixelWidth = tPixelWidth;
+      BuiltinEditTextActor.prototype.pixelHeight = tPixelHeight;
+
+      return BuiltinEditTextActor;
+    })(theatre.crews.swf.actors.TextActor);
+
+    BuiltinEditTextActor.prototype.displayListId = tId;
+
+    this.setActorRenderableCache(tId, new CanvasRenderable(tCanvas));
   };
 
 }(this));
---
+++ src/props/EditTextRenderProp.js
@@ -0,0 +1,109 @@
+/**
+ * @author Kuu Miyazaki
+ *
+ * Copyright (C) 2013 SWFCrew Project.
+ * This code is licensed under the zlib license. See LICENSE for details.
+ */
+(function(global) {
+
+  var theatre = global.theatre;
+  var mProps = theatre.crews.swf.props;
+  var mShapeUtils = theatre.crews.swf.utils.shape;
+  var Canvas = global.benri.draw.Canvas;
+  var Glyph = global.benri.draw.Glyph;
+
+  function createGlyph(pCharCode, pSwfShape, pAdvance, pMediaLoader) {
+    var tTempCanvas = new Canvas(1024, 1024);
+    var tRect = mShapeUtils.drawShape(pSwfShape, tTempCanvas, pMediaLoader);
+    var tGlyph = new Glyph(pCharCode);
+    tGlyph.data = tTempCanvas.getRecords(true);
+    tGlyph.advance = pAdvance;
+    tGlyph.rect = tRect;
+    return tGlyph;
+  }
+
+
+  /**
+   * The class responsible for rendering EditTexts.
+   * @class
+   * @extends {theatre.crews.swf.props.DisplayListRenderProp}
+   */
+  var EditTextRenderProp = (function(pSuper) {
+    function EditTextRenderProp(pWidth, pHeight) {
+      pSuper.call(this);
+      this.rebuildGlyph = true;
+    }
+
+    EditTextRenderProp.prototype = Object.create(pSuper.prototype);
+    EditTextRenderProp.prototype.constructor = EditTextRenderProp;
+
+    /**
+     * @inheritDoc
+     */
+    EditTextRenderProp.prototype.render = function(pData) {
+      // If super returns false, we abort rendering.
+      if (pSuper.prototype.render.call(this, pData) === false) {
+        return false;
+      }
+
+      var tActor = this.actor;
+      var tContext = this.context;
+
+      /**
+       * We have previously created a cache for this prop. Grab it.
+       * We created it in the Handler for Texts (DefineText/DefineEditText).
+       * @type {benri.render.Renderable}
+       */
+      var tRenderable = tActor.player.loader.getActorRenderableCache(tActor.displayListId);
+
+      var tCanvas = tRenderable.canvas;
+      var tString = tActor.text;
+      var tFont = tActor.font;
+      var tStyle = tActor.style;
+      var tCharCode, tFontInfo, tAdvance,
+          tGlyph, tShape;
+
+      if (tActor.device === false && this.rebuildGlyph) {
+        // Create glyphs.
+        for (var i = 0, il = tString.length; i < il; i++) {
+          tCharCode = tString.charCodeAt(i);
+
+          // Get advance.
+          tFontInfo = tActor.lookupTable[tCharCode + ''];
+          tAdvance = tFontInfo ? tFontInfo.advance
+                : (tCharCode < 256 ? tActor.fontHeight / 2 : tActor.fontHeight);
+          // Get glyph data.
+          tGlyph = tFont.getGlyph(tCharCode);
+          if (!tGlyph && tFontInfo) {
+            tShape = tFontInfo.shape;
+            tShape.fillStyles[0].color = tActor.color;
+            tGlyph = createGlyph(tCharCode, tShape, tAdvance, tActor.mediaLoader);
+            tFont.setGlyph(tCharCode, tGlyph);
+          }
+        }
+      }
+      // Draw text.
+      tCanvas.drawText(tString, tStyle);
+
+      // Offset by the Texts bounds.
+      // We do this because when boudns are negative they would be
+      // drawn off of the Canvas.
+      // To counteract that, in the drawing code we have translated
+      // by the negative of the bounds.
+      // Here we offset that negative translation to bring it back in place.
+      tContext.translate(tActor.bounds.left, tActor.bounds.top);
+
+      // We are rendering a bitmap in the end, so revert the matrix back to normal
+      // temporarily.
+      tContext.scale(20, 20);
+
+      // Render the cached Renderable.
+      tRenderable.render(tContext);
+    };
+
+    return EditTextRenderProp;
+  })(theatre.crews.swf.props.DisplayListRenderProp);
+
+  mProps.EditTextRenderProp = EditTextRenderProp;
+
+}(this));
